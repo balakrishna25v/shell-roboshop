6f303edaf21970cbef5068ca1e6960d0b85d07be
balu <balu.v25@gmail.com>
2026-02-22 19:51:24 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -6,10 +6,29 @@ AMI_ID "ami-0220d79f3f480ecf5" # replace with your AMI id
 
 
 for instance in $0
+
 do
-    aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --tag-specifications 
-    "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].InstanceId[0].
-    PrivateIpAddress' --output text 
-    done
+    instance-id = $(aws ec2 run-instances \
+   --image-id $AMI_ID \
+   --instance-type "t3.micro" \
+   --security-group-ids $SG_ID \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+   --query 'Instances[0].InstanceId' \
+   --output text )
 
-
+   if [ $instance == "frontend" ]; then
+      IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[0].Instances[0].PublicIpAddress' \
+        --output text
+      )
+    else
+      IP=$(
+        aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+        --output text
+      )     
+   fi     
+done
