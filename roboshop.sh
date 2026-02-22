4002faf611c2e343e5795e3d064e8df3496e3bdb
balu <balu.v25@gmail.com>
2026-02-22 20:04:29 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # This is the test run
 
-SG_ID "sg-0b0c61a02e2106b1f" # replace with your security group id
-AMI_ID "ami-0220d79f3f480ecf5" # replace with your AMI id
-
-
+SG_ID="sg-0b0c61a02e2106b1f" # replace with your security group id
+AMI_ID="ami-0220d79f3f480ecf5" # replace with your AMI id
 for instance in $0
 
 do
