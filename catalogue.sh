b5b711c9c5ca2f7ed6db9beba06afb262554b908
balu <balu.v25@gmail.com>
2026-02-28 02:12:06 +0530

    action

---
+++ catalogue.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[33m" # Yellow color
+N="\e[0m"  # No color
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script  root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+# Create logs directory if it doesn't exist
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){ 
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ....$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ....$G SUCCESS$N" | tee -a $LOGS_FILE
+    fi
+
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Disabling Nodejs Default version"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling Nodejs 20 version"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "creating system user"
+
+mkdir /app &>> $LOGS_FILE
+VALIDATE $? "creating application directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VaLIDATE $? "downloading application code"
